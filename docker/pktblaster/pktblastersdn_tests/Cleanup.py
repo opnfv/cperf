60a5919313fa5e577d9efd0aa395560f98008f6c
tonukasr <tonuka.sinharay@veryxtech.com>
2017-02-28 15:48:08 +0530

    Update Veryx-Pktblaster-sdn
    All Patch Set-1 comments has been addressed.
    
    Change-Id: I027b895908bcdfe756d69ad858890ee8398c88ea
    Signed-off-by: Tonuka Sinharay <tonuka.sinharay@veryxtech.com>
    Signed-off-by: tonukasr <tonuka.sinharay@veryxtech.com>

--- docker/pktblaster/pktblastersdn_tests/Cleanup.py
+++ docker/pktblaster/pktblastersdn_tests/Cleanup.py
@@ -1,7 +1,7 @@
-from sdnconfig import *
-
-import requests,json
+import requests
+import json
 import os
+import sdnconfig
 
 ### Stop Emulation Test.
 
@@ -9,14 +9,13 @@ fp = open("/tmp/temp.txt","r")
 test_id = fp.read()
 
 def vx_sdn_stop_emulation ():
-	
-        session_ip = emulation_host_ip  
+	session_ip = sdnconfig.emulation_host_ip
         url = "http://"+session_ip+"/VxSDN/emulation/test/stop"
         post_header = {'Content-Type': 'application/json','accept': 'application/json'}
         payload = {
-                       "username":"admin",
-                       "password":"admin",
-		       "id":test_id}
+                    "username":"admin",
+                    "password":"admin",
+                    "id":test_id }
         r=requests.post(url,json.dumps(payload),headers=post_header)
         output = json.loads(r.content)
         print "\n Emulation %s " %output['message']
